3078d4dda5b8ee6cce71d8eeaa7ceea61c3cdc20
Gabrieliusv <gabrielius.svi@gmail.com>
2019-10-21 14:37:43 +0300

    Added responsive Navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Button, Toolbar, AppBar, Box } from '@material-ui/core';
+import {
+  Typography,
+  Button,
+  Toolbar,
+  AppBar,
+  Box,
+  Hidden,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  Divider
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -31,11 +44,23 @@ const useStyles = makeStyles(theme => ({
     textDecoration: 'none',
     color: 'black',
     padding: 7
+  },
+  listItem__link: {
+    textDecoration: 'none',
+    color: 'black',
+    width: '100%',
+    textAlign: 'center',
+    padding: 7
   }
 }));
 
 function Navbar() {
   const classes = useStyles();
+  const [drawer, setDrawer] = useState(false);
+
+  const toggleDrawer = () => {
+    setDrawer(!drawer);
+  };
 
   return (
     <div className={classes.root}>
@@ -44,42 +69,91 @@ function Navbar() {
           <Typography variant='h5' className={classes.title}>
             MENO RESTAURATORIAI
           </Typography>
-          <Box className={classes.navigation}>
-            <NavLink
-              to='/'
-              exact
-              activeClassName={classes.active}
-              className={classes.link}
+          <Hidden smDown>
+            <Box className={classes.navigation}>
+              <NavLink
+                to='/'
+                exact
+                activeClassName={classes.active}
+                className={classes.link}
+              >
+                Titulinis
+              </NavLink>
+              <NavLink
+                to='/restauratoriai'
+                activeClassName={classes.active}
+                className={classes.link}
+              >
+                Restauratoriai
+              </NavLink>
+              <NavLink
+                to='/about'
+                className={classes.link}
+                activeClassName={classes.active}
+              >
+                Apie projektą
+              </NavLink>
+              <NavLink
+                to='/blog'
+                className={classes.link}
+                activeClassName={classes.active}
+              >
+                Blog'as
+              </NavLink>
+            </Box>
+            <Button color='inherit' className={classes.button}>
+              <Link to='/login' className={classes.button__link}>
+                Prisijungti
+              </Link>
+            </Button>
+          </Hidden>
+          <Hidden mdUp>
+            <IconButton
+              onClick={toggleDrawer}
+              color='inherit'
+              aria-label='menu'
             >
-              Titulinis
-            </NavLink>
-            <NavLink
-              to='/restauratoriai'
-              activeClassName={classes.active}
-              className={classes.link}
+              <MenuIcon />
+            </IconButton>
+          </Hidden>
+          <Drawer anchor='top' open={drawer} onClose={toggleDrawer}>
+            <div
+              role='presentation'
+              onClick={toggleDrawer}
+              onKeyDown={toggleDrawer}
             >
-              Restauratoriai
-            </NavLink>
-            <NavLink
-              to='/about'
-              className={classes.link}
-              activeClassName={classes.active}
-            >
-              Apie projektą
-            </NavLink>
-            <NavLink
-              to='/blog'
-              className={classes.link}
-              activeClassName={classes.active}
-            >
-              Blog'as
-            </NavLink>
-          </Box>
-          <Button color='inherit' className={classes.button}>
-            <Link to='/login' className={classes.button__link}>
-              Login
-            </Link>
-          </Button>
+              <List>
+                <ListItem button className={classes.button}>
+                  <Link to='/' className={classes.listItem__link}>
+                    Titulinis
+                  </Link>
+                </ListItem>
+                <ListItem button className={classes.button}>
+                  <Link to='/restauratoriai' className={classes.listItem__link}>
+                    Restauratoriai
+                  </Link>
+                </ListItem>
+                <ListItem button className={classes.button}>
+                  <Link to='/about' className={classes.listItem__link}>
+                    Apie projektą
+                  </Link>
+                </ListItem>
+                <ListItem button className={classes.button}>
+                  <Link to='/blog' className={classes.listItem__link}>
+                    Blog'as
+                  </Link>
+                </ListItem>
+              </List>
+              <Divider />
+              <List>
+                <ListItem button className={classes.button}>
+                  <Link to='/login' className={classes.listItem__link}>
+                    Prisijungti
+                  </Link>
+                </ListItem>
+              </List>
+            </div>
+          </Drawer>
         </Toolbar>
       </AppBar>
     </div>
--- client/src/components/layout/UserNavbar.js
+++ client/src/components/layout/UserNavbar.js
@@ -10,9 +10,11 @@ import {
   Box,
   IconButton,
   Menu,
-  MenuItem
+  MenuItem,
+  Hidden
 } from '@material-ui/core';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import ExitIcon from '@material-ui/icons/ExitToApp';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -34,14 +36,23 @@ const useStyles = makeStyles(theme => ({
     color: '#2C3B4E',
     borderBottom: '2px solid #2C3B4E'
   },
-  button: {
-    padding: 0,
-    margin: 0
-  },
-  button__link: {
+  menuItem__link: {
     textDecoration: 'none',
     color: 'black',
-    padding: 7
+    padding: 16,
+    width: '100%'
+  },
+  menuItem: {
+    padding: 0
+  },
+  menuItemHidden: {
+    padding: 0,
+    [theme.breakpoints.up('md')]: {
+      display: 'none'
+    }
+  },
+  logout: {
+    padding: 16
   }
 }));
 
@@ -65,37 +76,39 @@ function Navbar({ logout }) {
           <Typography variant='h5' className={classes.title}>
             MENO RESTAURATORIAI
           </Typography>
-          <Box className={classes.navigation}>
-            <NavLink
-              to='/'
-              exact
-              activeClassName={classes.active}
-              className={classes.link}
-            >
-              Titulinis
-            </NavLink>
-            <NavLink
-              to='/restauratoriai'
-              activeClassName={classes.active}
-              className={classes.link}
-            >
-              Restauratoriai
-            </NavLink>
-            <NavLink
-              to='/about'
-              className={classes.link}
-              activeClassName={classes.active}
-            >
-              Apie projektą
-            </NavLink>
-            <NavLink
-              to='/blog'
-              className={classes.link}
-              activeClassName={classes.active}
-            >
-              Blog'as
-            </NavLink>
-          </Box>
+          <Hidden smDown>
+            <Box className={classes.navigation}>
+              <NavLink
+                to='/'
+                exact
+                activeClassName={classes.active}
+                className={classes.link}
+              >
+                Titulinis
+              </NavLink>
+              <NavLink
+                to='/restauratoriai'
+                activeClassName={classes.active}
+                className={classes.link}
+              >
+                Restauratoriai
+              </NavLink>
+              <NavLink
+                to='/about'
+                className={classes.link}
+                activeClassName={classes.active}
+              >
+                Apie projektą
+              </NavLink>
+              <NavLink
+                to='/blog'
+                className={classes.link}
+                activeClassName={classes.active}
+              >
+                Blog'as
+              </NavLink>
+            </Box>
+          </Hidden>
           <IconButton
             aria-label='account of current user'
             aria-controls='menu-appbar'
@@ -120,12 +133,30 @@ function Navbar({ logout }) {
             open={open}
             onClose={handleClose}
           >
-            <MenuItem>
-              <Link to='/profile' className={classes.button__link}>
+            <MenuItem className={classes.menuItemHidden}>
+              <Link to='/' className={classes.menuItem__link}>
+                Titulinis
+              </Link>
+            </MenuItem>
+            <MenuItem className={classes.menuItemHidden}>
+              <Link to='/restauratoriai' className={classes.menuItem__link}>
+                Restauratoriai
+              </Link>
+            </MenuItem>
+            <MenuItem className={classes.menuItemHidden}>
+              <Link to='/blog' className={classes.menuItem__link}>
+                Blog'as
+              </Link>
+            </MenuItem>
+            <MenuItem className={classes.menuItem}>
+              <Link to='/profile' className={classes.menuItem__link}>
                 Profile
               </Link>
             </MenuItem>
-            <MenuItem onClick={logout}>Logout</MenuItem>
+            <MenuItem className={classes.logout} onClick={logout}>
+              Logout
+              <ExitIcon />
+            </MenuItem>
           </Menu>
         </Toolbar>
       </AppBar>
