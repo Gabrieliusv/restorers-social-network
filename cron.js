6fba8f3af2ff9d06fabb9718bd5252861bd752b9
Gabrieliusv <gabrielius.svi@gmail.com>
2019-11-21 19:03:45 +0200

    Added cron to schedule cleanup of temporary files

---
+++ cron.js
@@ -0,0 +1,40 @@
+const CronJob = require("cron").CronJob;
+const fs = require("fs");
+
+//Executes cleanup of temporary files on monday 00:00
+const job = new CronJob("00 00 00 * * 1", function() {
+  if (process.env.NODE_ENV === "production") {
+    fs.readdir("./client/build/temp/", (err, files) => {
+      files.forEach(file => {
+        //Check date of upload
+        fs.stat(`./client/build/temp/${file}`, function(err, stats) {
+          var mtime =
+            (new Date().getTime() - new Date(stats.mtime).getTime()) / 3600000;
+          //Delete if older than 24h
+          if (mtime > 24) {
+            fs.unlink(`./client/public/temp/${file}`, function(err) {
+              if (err) throw err;
+            });
+          }
+        });
+      });
+    });
+  } else {
+    fs.readdir("./client/public/temp/", (err, files) => {
+      files.forEach(file => {
+        //Check date of upload
+        fs.stat(`./client/public/temp/${file}`, function(err, stats) {
+          var mtime =
+            (new Date().getTime() - new Date(stats.mtime).getTime()) / 3600000;
+          //Delete if older than 24h
+          if (mtime > 24) {
+            fs.unlink(`./client/public/temp/${file}`, function(err) {
+              if (err) throw err;
+            });
+          }
+        });
+      });
+    });
+  }
+});
+job.start();
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const connectDB = require('./db');
-const path = require('path');
+const express = require("express");
+const connectDB = require("./db");
+const path = require("path");
+const cron = require("./cron");
 
 const app = express();
 
@@ -11,18 +12,18 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 //Use Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/blog', require('./routes/api/blog'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/blog", require("./routes/api/blog"));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
